d0f0e2213e36fb688a059d133259e25f901e96db
Minh Vu <minhvh99@gmail.com>
2021-05-10 22:57:36 +0700

    Add omnisharp language server

--- vim/.config/nvim/lua/lsp.lua
+++ vim/.config/nvim/lua/lsp.lua
@@ -106,8 +106,16 @@ end
 
 -- Use a loop to conveniently both setup defined servers 
 -- and map buffer local keybindings when the language server attaches
-local servers = { 'clangd', 'gopls', 'pyright', 'tsserver' }
+local servers = { 'clangd', 'gopls', 'pyright', 'tsserver', 'gdscript', 'rust_analyzer' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
 
+-- Omnisharp
+local pid = vim.fn.getpid()
+local omnisharp_path = '/usr/bin/omnisharp'
+
+nvim_lsp['omnisharp'].setup {
+  cmd = { omnisharp_path, "--languageserver" , "--hostPID", tostring(pid) },
+  on_attach = on_attach
+}
