482ec748dd562888bd078f49cc4af03ed3adc6ef
MinhVH <minhvh99@gmail.com>
2021-08-17 23:46:21 +0700

    Vim,Neovim: Use onedark cterm values for termguicolors (except special_grey)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -52,12 +52,27 @@ vim.api.nvim_command 'PackerInstall'
 ----------------------------------------
 -- COLOR SCHEME                        -
 ----------------------------------------
--- override some color
--- prefer some color from cterm instead of gui
+-- override onedark color
+-- override gui values with corresponding cterm values (except for special_grey)
 vim.g['onedark_color_overrides'] = {
-	background = { gui = "#262626", cterm = "235", cterm16 = "0" },
+	red = { gui = "#ff5f87", cterm = "204", cterm16 = "1" },
+	dark_red = { gui = "#ff0000", cterm = "196", cterm16 = "9" },
+	green = { gui = "#87d787", cterm = "114", cterm16 = "2" },
+	yellow = { gui = "#d7af87", cterm = "180", cterm16 = "3" },
+	dark_yellow = { gui = "#d7875f", cterm = "173", cterm16 = "11" },
+	blue = { gui = "#00afff", cterm = "39", cterm16 = "4" },
+	purple = { gui = "#d75fd7", cterm = "170", cterm16 = "5" },
+	cyan = { gui = "#00afd7", cterm = "38", cterm16 = "6" },
+	white = { gui = "#afafaf", cterm = "145", cterm16 = "15" },
+	black = { gui = "#262626", cterm = "235", cterm16 = "0" },
+	foreground = { gui = "#afafaf", cterm = "145", cterm16 = "NONE" },
+	background = { gui = "#262626", cterm = "235", cterm16 = "NONE" },
+	comment_grey = { gui = "#5f5f5f", cterm = "59", cterm16 = "7" },
+	gutter_fg_grey = { gui = "#444444", cterm = "238", cterm16 = "8" },
 	cursor_grey = { gui = "#303030", cterm = "236", cterm16 = "0" },
 	visual_grey = { gui = "#3a3a3a", cterm = "237", cterm16 = "8" },
+	menu_grey = { gui = "#3a3a3a", cterm = "237", cterm16 = "7" },
+	special_grey = { gui = "#3b4048", cterm = "238", cterm16 = "7" }, -- use default onedark color
 	vertsplit = { gui = "#5f5f5f", cterm = "59", cterm16 = "7" },
 }
 
