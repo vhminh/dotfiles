cf6cb66c722fdea8046a274a2c0be3a381aaed45
Minh Vu <minhvh99@gmail.com>
2022-10-30 11:43:34 +0700

    Neovim: Migrate nvim-lsp-installer to mason.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -27,7 +27,8 @@ packer.startup(function()
   use 'joshdick/onedark.vim'
   use 'neovim/nvim-lspconfig'
   use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-path', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-vsnip', 'hrsh7th/vim-vsnip', 'rafamadriz/friendly-snippets' } }
-  use { 'williamboman/nvim-lsp-installer' }
+  use { 'williamboman/mason.nvim' }
+  use { "williamboman/mason-lspconfig.nvim" }
   use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' }
   use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } } }
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
@@ -208,7 +209,8 @@ end
 
 -- install language servers
 local servers = { 'sumneko_lua', 'gopls', 'clangd', 'pyright' }
-require('nvim-lsp-installer').setup {
+require('mason').setup()
+require('mason-lspconfig').setup {
   ensure_installed = servers,
 }
 
