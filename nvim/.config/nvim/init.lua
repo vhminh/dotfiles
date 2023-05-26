306d029cc33bf99df95268298264e5857c1c5612
Minh Vu <mvu@axon.com>
2023-05-26 17:00:20 +0700

    Neovim: minor metals config change

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -243,12 +243,11 @@ metals_config.settings = {
   showImplicitArguments = true,
   excludedPackages = { 'akka.actor.typed.javadsl', 'com.github.swagger.akka.javadsl' },
 }
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
 metals_config.on_attach = on_attach
 local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'scala', 'sbt' },
+  pattern = { 'scala', 'sbt', 'java' },
   callback = function()
     require('metals').initialize_or_attach(metals_config)
   end,
