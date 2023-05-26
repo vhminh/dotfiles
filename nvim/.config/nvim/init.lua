6b875e181fd80c870c632d0f560208df3c2a6d01
Minh Vu <mvu@axon.com>
2023-05-26 16:58:30 +0700

    Neovim: nvim-tree group empty dirs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -114,6 +114,9 @@ require 'nvim-tree'.setup {
   diagnostics = {
     enable = true,
   },
+  renderer = {
+    group_empty = true,
+  },
 }
 vim.keymap.set('n', '<leader>e', '<Cmd>NvimTreeToggle<CR>')
 
