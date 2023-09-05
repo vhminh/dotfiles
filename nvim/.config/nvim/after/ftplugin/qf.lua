06f095d2ba425a1ef25139acd30764c890426619
Minh Vu <minhvh99@gmail.com>
2023-09-06 01:16:23 +0700

    nvim: correct mapping to close quickfix

--- nvim/.config/nvim/after/ftplugin/qf.lua
+++ nvim/.config/nvim/after/ftplugin/qf.lua
@@ -1,4 +1,4 @@
 vim.keymap.set('n', 'j', 'j<CR><C-w>p', { buffer = true })
 vim.keymap.set('n', 'k', 'k<CR><C-w>p', { buffer = true })
-vim.keymap.set('n', 'o', '<CR><C-w>p:q<CR>', { buffer = true })
-vim.keymap.set('n', 'q', '<Cmd>q<CR>', { buffer = true })
+vim.keymap.set('n', 'o', '<CR><Cmd>cclose<CR>', { buffer = true })
+vim.keymap.set('n', 'q', '<Cmd>cclose<CR>', { buffer = true })
