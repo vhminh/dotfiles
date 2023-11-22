ee51a53efbbb7f63e144a4c2be274e3ec6f60784
Minh Vu <mvu@axon.com>
2023-11-23 00:38:21 +0700

    neovim: simplify colorscheme

--- nvim/.config/nvim/after/queries/go/highlights.scm
+++
@@ -1,1 +0,0 @@
-(package_identifier) @package.name
--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,28 +1,43 @@
-local colors = require('colors')
+local colors = require('colors').raw
 
 vim.g['onedark_color_overrides'] = {
-  background = colors.raw.background,
-  comment_grey = colors.raw.light_grey,
-  cursor_grey = colors.raw.slightly_light_grey,
-  visual_grey = colors.raw.grey,
-  menu_grey = colors.raw.grey,
-  vertsplit = colors.raw.light_grey,
+  background = colors.background,
+  comment_grey = colors.light_grey,
+  cursor_grey = colors.slightly_light_grey,
+  visual_grey = colors.grey,
+  menu_grey = colors.grey,
+  vertsplit = colors.light_grey,
 }
 
-vim.cmd([[
-if (has('autocmd') && !has('gui_running'))
-augroup colorextend
-autocmd!
-let colors = onedark#GetColors()
-autocmd ColorScheme * call onedark#extend_highlight('Keyword', { 'fg': colors.purple })
-autocmd ColorScheme * call onedark#extend_highlight('Identifier', { 'fg': colors.white })
-augroup END
-endif
-]])
+local function onedark_set_color(hl_group, color)
+  vim.fn['onedark#extend_highlight'](hl_group, { fg = color })
+end
 
-vim.cmd([[
-augroup highlight_vertsplit
-autocmd!
-autocmd Colorscheme * highlight vertsplit guifg=Gray guibg=bg
-augroup END
-]])
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = '*',
+  callback = function()
+    onedark_set_color('Identifier', colors.white)
+    onedark_set_color('Function', colors.blue)
+    onedark_set_color('Keyword', colors.purple)
+    onedark_set_color('PreProc', colors.purple)
+    onedark_set_color('Include', colors.purple)
+    onedark_set_color('PreCondit', colors.purple)
+    onedark_set_color('Type', colors.white)
+    onedark_set_color('StorageClass', colors.purple)
+    onedark_set_color('Structure', colors.purple)
+    onedark_set_color('Typedef', colors.cyan)
+
+    -- vim.api.nvim_set_hl(0, '@lsp.type.class', { fg = colors.yellow.gui })
+    vim.api.nvim_set_hl(0, '@lsp.type.class', { link = 'Identifier' })
+    vim.api.nvim_set_hl(0, '@lsp.type.enum', { link = 'Identifier' })
+    -- vim.api.nvim_set_hl(0, '@lsp.type.interface', { fg = colors.yellow.gui })
+    vim.api.nvim_set_hl(0, '@lsp.type.interface', { link = 'Identifier' })
+    vim.api.nvim_set_hl(0, '@lsp.type.namespace', { link = 'Identifier' })
+    vim.api.nvim_set_hl(0, '@lsp.type.struct', { link = 'Identifier' })
+    -- vim.api.nvim_set_hl(0, '@lsp.type.type', { fg = colors.yellow.gui })
+    vim.api.nvim_set_hl(0, '@lsp.type.type', { link = 'Identifier' })
+    vim.api.nvim_set_hl(0, '@type.builtin', { link = 'Keyword' })
+
+    vim.api.nvim_set_hl(0, 'vertsplit', { fg = 'Gray' })
+  end,
+})
