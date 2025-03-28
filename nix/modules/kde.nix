744680ca003141cee72da1938cea93a6580db74b
Minh Vu <minhvh99@gmail.com>
2025-03-29 01:16:00 +0700

    nix: don't run sddm under wayland

--- nix/modules/kde.nix
+++ nix/modules/kde.nix
@@ -5,7 +5,7 @@
 
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.enable = false;
   services.desktopManager.plasma6.enable = true;
 
   services.displayManager.defaultSession = "plasma";
