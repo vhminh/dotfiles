1341df38af195d9d41ad53ceda0516d1e5c68f9e
Minh Vu <mvu@axon.com>
2025-06-05 12:51:23 +0700

    neovim: lsp support in statusline

--- nvim/.config/nvim/lua/statusline.lua
+++ nvim/.config/nvim/lua/statusline.lua
@@ -78,33 +78,128 @@ components.fileformat = function()
   return vim.bo.fileformat
 end
 
+vim.api.nvim_create_augroup('statusline.lsp_diagnostics', { clear = true })
+vim.api.nvim_create_autocmd('DiagnosticChanged', {
+  group = 'statusline.lsp_diagnostics',
+  callback = function(args)
+    local bufnr = args.buf
+    local diagnostics = vim.diagnostic.count(bufnr)
+    vim.b[bufnr].stl_lsp_diagnostics = diagnostics
+  end,
+})
+
+---@generic T
+---@param value vim.NIL|T?
+---@param default T
+---@return T
+local function nil_safe(value, default)
+  if value == nil or value == vim.NIL then
+    return default
+  end
+  return value
+end
+
+components.lsp_diagnostics = function()
+  local diagnostics = nil_safe(vim.b.stl_lsp_diagnostics, {})
+
+  local error = nil_safe(diagnostics[vim.diagnostic.severity.ERROR], 0)
+  local warn = nil_safe(diagnostics[vim.diagnostic.severity.WARN], 0)
+  local info = nil_safe(diagnostics[vim.diagnostic.severity.INFO], 0)
+  local hint = nil_safe(diagnostics[vim.diagnostic.severity.HINT], 0)
+  local strs = {}
+  if error > 0 then
+    table.insert(strs, string.format(' %d', error))
+  end
+  if warn > 0 then
+    table.insert(strs, string.format(' %d', warn))
+  end
+  if info > 0 then
+    table.insert(strs, string.format(' %d', info))
+  end
+  if hint > 0 then
+    table.insert(strs, string.format(' %d', hint))
+  end
+  return table.concat(strs, ' ')
+end
+
+---@param lsp_clients { id: integer, name: string }[]
+local function make_string(lsp_clients)
+  local names = {}
+  for _, client in ipairs(lsp_clients) do
+    table.insert(names, client.name)
+  end
+  return table.concat(names, ',')
+end
+
+-- we have to keep track of attached clients ourselves
+-- because vim.lsp.get_clients() doesn't give the latest list on LspDetach
+-- https://github.com/neovim/neovim/pull/20148
+vim.api.nvim_create_augroup('statusline.lsp_clients', { clear = true })
+vim.api.nvim_create_autocmd({ 'LspAttach', 'LspDetach' }, {
+  group = 'statusline.lsp_clients',
+  callback = function(args)
+    local bufnr = args.buf
+    local active_clients = vim.b[bufnr].lsp_clients or {}
+    local client_id = args.data.client_id
+    if args.event == 'LspAttach' then
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client then
+        table.insert(active_clients, { id = client.id, name = client.name })
+      end
+    elseif args.event == 'LspDetach' then
+      for i, c in ipairs(active_clients) do
+        if c.id == client_id then
+          table.remove(active_clients, i)
+          break
+        end
+      end
+    end
+    vim.b[bufnr].stl_lsp_clients = active_clients
+    vim.b[bufnr].stl_lsp_clients_str = make_string(active_clients)
+  end,
+})
+
+components.lsp_clients = function()
+  local str = vim.b.stl_lsp_clients_str
+  if str == vim.NIL or str == nil or str == '' then
+    return ''
+  end
+  return string.format(' %s', str)
+end
+
 components.git = function()
   local dict = vim.b.gitsigns_status_dict or {}
-  local str = {}
+  local t = {}
   if dict['head'] ~= nil then
-    table.insert(str, string.format(' %s', dict['head']))
+    table.insert(t, string.format(' %s', dict['head']))
   end
   if dict['added'] and dict['added'] ~= 0 then
-    table.insert(str, string.format('+%s', dict['added']))
+    table.insert(t, string.format('+%d', dict['added']))
   end
   if dict['changed'] and dict['changed'] ~= 0 then
-    table.insert(str, string.format('~%s', dict['changed']))
+    table.insert(t, string.format('~%d', dict['changed']))
   end
   if dict['removed'] and dict['removed'] ~= 0 then
-    table.insert(str, string.format('-%s', dict['removed']))
+    table.insert(t, string.format('-%d', dict['removed']))
   end
-  return table.concat(str, ' ')
+  if next(t) == nil then
+    return ''
+  end
+  return string.format(' %s', table.concat(t, ' '))
 end
 
 _G.statusline = {}
 
 statusline.active = function()
   return string.format(
-    ' %s %%t %s %%= %%= %%l / %%L %s %s %s ▊',
+    ' %s %%t %s %s %%= %%= %%l / %%L %s %s%s%s ▊',
     components.mode(),
     components.buf_flags(),
+    components.lsp_diagnostics(),
     components.fileencoding(),
     components.fileformat(),
+    -- optional components, they already have padding spaces
+    components.lsp_clients(),
     components.git()
   )
 end
