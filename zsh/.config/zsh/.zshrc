d091b17a6a0636ff802b26b9e72ce888407f2466
Minh Vu <minhvh99@gmail.com>
2025-07-29 01:28:27 +0700

    zsh: add optional checks

--- zsh/.config/zsh/.zshrc
+++ zsh/.config/zsh/.zshrc
@@ -5,9 +5,11 @@ for file in $ZDOTDIR/rc/*.zsh; do
   source $file
 done
 
-for file in $ZDOTDIR/host_specific/*.zsh; do
-  source $file
-done
+if [[ -d $ZDOTDIR/host_specific ]] then
+  for file in $ZDOTDIR/host_specific/*.zsh; do
+    source $file
+  done
+fi
 
 if [[ -f $ZDOTDIR/host_specific.zsh ]]; then
   source $ZDOTDIR/host_specific.zsh
--- zsh/.config/zsh/rc/homebrew.zsh
+++ zsh/.config/zsh/rc/homebrew.zsh
@@ -4,10 +4,14 @@ fi
 
 if command -v brew >/dev/null 2>&1; then
   # rustup
-  export PATH=$(brew --prefix rustup)/bin:$PATH
+  if command -v rustup >/dev/null 2>&1; then
+    export PATH=$(brew --prefix rustup)/bin:$PATH
+  fi
 
   # sdkman
-  export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
-  [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+  if command -v sdk >/dev/null 2>&1; then
+    export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
+    [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+  fi
 fi
 
