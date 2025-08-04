b2d0f1b78677bcc50892b2bfa8d30c70ef164e61
Minh Vu <minhvh99@gmail.com>
2025-08-05 01:31:42 +0700

    zsh: correct brew package optional checks

--- zsh/.config/zsh/rc/homebrew.zsh
+++ zsh/.config/zsh/rc/homebrew.zsh
@@ -1,15 +1,13 @@
 if [[ -x /opt/homebrew/bin/brew ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
-fi
 
-if command -v brew >/dev/null 2>&1; then
   # rustup
-  if command -v rustup >/dev/null 2>&1; then
+  if brew list --formula | grep -q rustup; then
     export PATH=$(brew --prefix rustup)/bin:$PATH
   fi
 
   # sdkman
-  if command -v sdk >/dev/null 2>&1; then
+  if brew list --formula | grep -q sdkman-cli; then
     export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
     [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
   fi
