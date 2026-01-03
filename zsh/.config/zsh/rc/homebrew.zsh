196ceed06b3ac9d9f1ae63ae33b138733509d502
Minh Vu <mvu@axon.com>
2026-01-03 18:12:13 +0700

    zsh: caching homebrew calls

--- zsh/.config/zsh/rc/homebrew.zsh
+++ zsh/.config/zsh/rc/homebrew.zsh
@@ -1,19 +1,23 @@
 if [[ -x /opt/homebrew/bin/brew ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 
+  # caching for better performance
+  brew_prefix=$(brew --prefix)
+  brew_installed_formulas=$(brew list --formula)
+
   # rustup
-  if brew list --formula | grep -q rustup; then
-    export PATH=$(brew --prefix rustup)/bin:$PATH
+  if echo $brew_installed_formulas | grep -q rustup; then
+    export PATH=${brew_prefix}/rustup/bin:$PATH
   fi
 
   # sdkman
-  if brew list --formula | grep -q sdkman-cli; then
-    export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
+  if echo $brew_installed_formulas | grep -q sdkman-cli; then
+    export SDKMAN_DIR=${brew_prefix}/sdkman-cli/libexec
     [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
   fi
 
   # mise
-  if brew list --formula | grep -q mise; then
+  if echo $brew_installed_formulas | grep -q mise; then
     eval "$(mise activate zsh)"
   fi
 fi
