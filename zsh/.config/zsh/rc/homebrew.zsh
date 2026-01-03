635157882ffb7939a17cb278cb717f4b460c25b8
Minh Vu <mvu@axon.com>
2026-01-03 21:18:44 +0700

    zsh: directly use $HOMEBREW_PREFIX instead of calling brew cli

--- zsh/.config/zsh/rc/homebrew.zsh
+++ zsh/.config/zsh/rc/homebrew.zsh
@@ -1,18 +1,14 @@
 if [[ -x /opt/homebrew/bin/brew ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 
-  # caching for better performance
-  brew_prefix=$(brew --prefix)
-  brew_installed_formulas=$(brew list --formula)
-
   # rustup
-  if echo $brew_installed_formulas | grep -q rustup; then
-    export PATH=${brew_prefix}/rustup/bin:$PATH
+  if [[ -d "$HOMEBREW_PREFIX/opt/rustup" ]]; then
+    export PATH=$HOMEBREW_PREFIX/opt/rustup/bin:$PATH
   fi
 
   # sdkman
-  if echo $brew_installed_formulas | grep -q sdkman-cli; then
-    export SDKMAN_DIR=${brew_prefix}/sdkman-cli/libexec
+  if [[ -d "$HOMEBREW_PREFIX/opt/sdkman-cli" ]]; then
+    export SDKMAN_DIR=$HOMEBREW_PREFIX/opt/sdkman-cli/libexec
     [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
   fi
 
