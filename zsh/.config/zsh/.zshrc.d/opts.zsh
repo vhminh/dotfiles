a1514ef7db94c71fb3d507d5462119c0c4d2f38e
Minh Vu <mvu@axon.com>
2026-03-03 01:06:53 +0700

    zsh: useful history opts

--- zsh/.config/zsh/.zshrc.d/opts.zsh
+++ zsh/.config/zsh/.zshrc.d/opts.zsh
@@ -1,8 +1,9 @@
-HISTSIZE=1000
-SAVEHIST=1000
+HISTSIZE=50000
+SAVEHIST=50000
 HISTFILE="$XDG_STATE_HOME/zsh/history"
 mkdir -p "$(dirname $HISTFILE)"
 setopt extendedglob nomatch notify
 bindkey -v
 unsetopt autocd beep
-setopt extended_history hist_ignore_all_dups
+setopt extended_history hist_ignore_all_dups hist_ignore_space
+setopt share_history hist_expire_dups_first hist_verify hist_reduce_blanks
