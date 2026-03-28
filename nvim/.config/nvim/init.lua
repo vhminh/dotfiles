5f7b57718b5793056d84efc1599d862cc48b9683
Minh Vu <mvu@axon.com>
2026-03-28 20:57:34 +0700

    neovim: add config for neovide

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,4 +2,7 @@ require('pre')
 require('plug')
 require('statusline')
 require('vimopts')
+if vim.g.neovide then
+  require('neovide')
+end
 require('post')
---
+++ nvim/.config/nvim/lua/neovide.lua
@@ -0,0 +1,14 @@
+vim.o.guifont = 'NotoSansM Nerd Font Mono:h18:w-1'
+vim.g.neovide_remember_window_size = false
+vim.g.neovide_refresh_rate_idle = 5
+vim.g.neovide_scroll_animation_length = 0.12
+vim.g.neovide_cursor_animation_length = 0.06
+vim.g.neovide_cursor_trail_size = 0.06
+
+vim.api.nvim_set_current_dir(vim.env.PWD)
+
+-- HACK: remove when neovide releases new version that supports `grid` option in config.toml
+vim.defer_fn(function()
+  vim.opt.columns = 128
+  vim.opt.lines = 36
+end, 20)
